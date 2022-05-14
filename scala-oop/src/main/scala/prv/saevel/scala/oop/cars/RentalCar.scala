89adc958e46b17b02af79d2f1f422781f81cfec3
adrianpusty <adrianpusty>
2022-05-14 14:35:03 +0200

    RentalCar class

--- scala-oop/src/main/scala/prv/saevel/scala/oop/cars/RentalCar.scala
+++ scala-oop/src/main/scala/prv/saevel/scala/oop/cars/RentalCar.scala
@@ -1,3 +1,3 @@
 package main.scala.prv.saevel.scala.oop.cars
 
-class RentalCar
+class RentalCar(private val serialNumber: Long, override val brand: String, override val model: String, override var owner: String, val rental: String) extends Car(serialNumber, brand, model, owner) {}
