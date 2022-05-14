87548bed35c3a02547928e3090324a9051a63ed5
adrianpusty <adrianpusty>
2022-05-14 14:52:07 +0200

    InMemoryCarRepository

--- scala-oop/src/main/scala/prv/saevel/scala/oop/cars/InMemoryCarRepository.scala
+++ scala-oop/src/main/scala/prv/saevel/scala/oop/cars/InMemoryCarRepository.scala
@@ -1,10 +1,10 @@
 package main.scala.prv.saevel.scala.oop.cars
 
-import prv.saevel.scala.oop.persistence.{InMemoryRepository}
+import main.scala.prv.saevel.scala.oop.persistence.InMemoryRepository
 
-object InMemoryCarRepository extends CarRepository with InMemoryRepository[Long, Car] /*{
-
+object InMemoryCarRepository extends CarRepository with InMemoryRepository[Long, Car]
+{
   override def findByBrand(brand: String): List[Car] = items.filter(car => car.brand == brand)
 
   override def findByModel(model: String): List[Car] = items.filter(car => car.model == model)
-}*/
+}
