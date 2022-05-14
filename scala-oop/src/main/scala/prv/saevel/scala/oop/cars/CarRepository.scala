ecf3c0c28126070c78cd8f93dccdfcec33a4013b
adrianpusty <adrianpusty>
2022-05-14 14:51:15 +0200

    CarRepository trait

--- scala-oop/src/main/scala/prv/saevel/scala/oop/cars/CarRepository.scala
+++ scala-oop/src/main/scala/prv/saevel/scala/oop/cars/CarRepository.scala
@@ -1,5 +1,9 @@
 package main.scala.prv.saevel.scala.oop.cars
 
-import prv.saevel.scala.oop.persistence.Repository
+import main.scala.prv.saevel.scala.oop.persistence.Repository
 
 trait CarRepository extends Repository[Long, Car]
+{
+  def findByBrand(brand: String): List[Car]
+  def findByModel(model: String): List[Car]
+}
