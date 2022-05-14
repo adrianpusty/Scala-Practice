926fb9e01023d210620031a74168655a539bdda0
adrianpusty <adrianpusty>
2022-05-14 14:24:10 +0200

    Car class

--- scala-oop/src/main/scala/prv/saevel/scala/oop/cars/Car.scala
+++ scala-oop/src/main/scala/prv/saevel/scala/oop/cars/Car.scala
@@ -1,11 +1,15 @@
 package main.scala.prv.saevel.scala.oop.cars
 
-import prv.saevel.scala.oop.persistence.PrimaryKey
+import main.scala.prv.saevel.scala.oop.persistence.PrimaryKey
 
-class Car extends PrimaryKey[Long] {
-  override val id: Long = ???
+import scala.util.Random
+
+class Car protected(private val serialNumber: Long, val brand: String, val model: String, var owner: String) extends PrimaryKey[Long]
+{
+  override val id: Long = serialNumber
 }
 
-object Car {
-  def apply(brand: String, model: String, owner: String): Car = ???
+object Car
+{
+  def apply(brand: String, model: String, owner: String): Car = new Car(Random.nextLong, brand, model, owner)
 }
