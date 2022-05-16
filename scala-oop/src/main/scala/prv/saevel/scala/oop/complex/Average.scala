8d776920466e1edd20e3486df541633ceb6d6eb0
adrianpusty <adrianpusty>
2022-05-16 08:58:11 +0200

    apply method in Average class

--- scala-oop/src/main/scala/prv/saevel/scala/oop/complex/Average.scala
+++ scala-oop/src/main/scala/prv/saevel/scala/oop/complex/Average.scala
@@ -2,5 +2,8 @@ package main.scala.prv.saevel.scala.oop.complex
 
 object Average {
 
-  def apply[T](data: List[T])(implicit addition: Addition[T], division: DivisionByInt[T]): T = ???
+  def apply[T](data: List[T])(implicit addition: Addition[T], division: DivisionByInt[T]): T = {
+    val sum = data.reduce((t1, t2) => addition.add(t1, t2))
+    division.divideBy(sum, data.length)
+  }
 }
